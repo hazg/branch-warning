f9825ffe995c0cd4922f0ad0fcdf190de3a1b860
hazg <hazg@mail.ru>
2024-04-07 20:23:14 +0300

    all done

--- src/extension.ts
+++ src/extension.ts
@@ -8,38 +8,61 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "branrning" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('branrning.helloWorld', () => {
+	//let disposable = vscode.commands.registerCommand('theme-by-branch.watch', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		const wb = vscode.workspace.getConfiguration('workbench')
 		const git = vscode.extensions.getExtension('vscode.git')
-		let origTheme = wb.get('colorTheme')
+		// let origTheme = wb.get('colorTheme')
 		const repo = git?.exports.getAPI(1).repositories[0]
+		// const theme = wb.get('themeByBranch.theme')
+		// const wb_color = new vscode.ThemeColor ( 'activityBar.background' )
+		/**
+ * Create and show a StatusBarItem
+ */
 
-		repo.state.onDidChange(() => {
-				
-				let branch = repo.state.HEAD?.name
+
+		let sbItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+		sbItem.text = "🚨 WARNING ⚠️";
+		sbItem.tooltip = `${repo.state.HEAD?.name} branch`;
+		sbItem.backgroundColor = new vscode.ThemeColor('statusBarItem.errorBackground');
+		
+		var lastBranch = ''
+		setState();
+
+		function setState() {
+			let branch = repo.state.HEAD?.name
+			
+			if(lastBranch != branch) {
+				lastBranch = branch
 				// TODO: await vscode.commands.executeCommand('workbench.action.previewColorTheme', { publisher, name, version }, settingsId);
 				// https://github.com/search?q=repo%3Amicrosoft%2Fvscode-theme-tester%20previewColorTheme&type=code
 				switch(branch) {
 					case 'main':
 					case 'master':
-						wb.update('colorTheme', 'Red', vscode.ConfigurationTarget.Workspace)
+						sbItem.show();
+						// wb.update('workbench.colorCustomizations.statusBar.background', '#F00')
+						// vscode.window.showInformationMessage(`Branch changed ${branch} ${theme}`)
+						// wb.update('colorTheme', theme, vscode.ConfigurationTarget.Workspace)
+						// "statusBar.background": "#938e04",
 						break;
 					default:
-						wb.update('colorTheme', origTheme, vscode.ConfigurationTarget.Workspace)
+						sbItem.hide();
+						// wb.update('colorTheme', origTheme, vscode.ConfigurationTarget.Workspace)
 				}
-				// vscode.window.showInformationMessage(`Branch changed ${theme}`)
 				
+			}
+		}
+		repo.state.onDidChange(() => {
+			setState();
 		})
 		
 		
-	}) 
+	//}) 
 		
 		// (e => {
 		// 	if (e.state.equals(vscode.GitRepositoryState.Idle)) {
@@ -52,11 +75,11 @@ export function activate(context: vscode.ExtensionContext) {
 		// 	}
 		// });
 		// context.subscriptions.push(onDidChangeGitRepository);
-		
+		//context.subscriptions.push(disposable);	
 		// vscode.commands.executeCommand("workbench.colorTheme", "Solarized Dark");
 	// });
 
-	context.subscriptions.push(disposable);
+	
 }
 
 // This method is called when your extension is deactivated
